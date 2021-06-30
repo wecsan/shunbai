badceef25a5ce08a2cb3b6c02c2f93e408e8b6f0
wecsyu <74102486+wecsan@users.noreply.github.com>
2021-06-30 22:31:50 +0800

    Update waifu-tips.js

--- waifu-tips.js
+++ waifu-tips.js
@@ -65,7 +65,7 @@ function loadWidget(config) {
 				window.ASTEROIDSPLAYERS.push(new Asteroids());
 			} else {
 				const script = document.createElement("script");
-				script.src = "https://cdn.jsdelivr.net/gh/stevenjoezhang/asteroids/asteroids.js";
+				script.src = "https://cdn.jsdelivr.net/gh//wecsan/shunbai/autoload.js";
 				document.head.appendChild(script);
 			}
 		});
