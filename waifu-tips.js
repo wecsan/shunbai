e0858f254951d5061caa8dd10d0aea48d4ad3c85
wecsyu <74102486+wecsan@users.noreply.github.com>
2021-06-30 22:33:12 +0800

    Update waifu-tips.js

--- waifu-tips.js
+++ waifu-tips.js
@@ -65,7 +65,7 @@ function loadWidget(config) {
 				window.ASTEROIDSPLAYERS.push(new Asteroids());
 			} else {
 				const script = document.createElement("script");
-				script.src = "https://cdn.jsdelivr.net/gh//wecsan/shunbai/autoload.js";
+				script.src = "https://cdn.jsdelivr.net/gh/wecsan/shunbai/autoload.js";
 				document.head.appendChild(script);
 			}
 		});
@@ -77,7 +77,7 @@ function loadWidget(config) {
 			Live2D.captureFrame = true;
 		});
 		document.querySelector("#waifu-tool .fa-info-circle").addEventListener("click", () => {
-			open("https://github.com/stevenjoezhang/live2d-widget");
+			open("http://www.badouxia.com/");
 		});
 		document.querySelector("#waifu-tool .fa-times").addEventListener("click", () => {
 			localStorage.setItem("waifu-display", Date.now());
